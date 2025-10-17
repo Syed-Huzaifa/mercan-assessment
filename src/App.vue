ebf2f7a6f95dc94c454f686e188cefc030e66620
Syed-Huzaifa <shahhuzaifa338@gmail.com>
2025-10-18 03:13:19 +0500

    Add Tailwind CSS integration and update styles

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-md font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <style scoped>
